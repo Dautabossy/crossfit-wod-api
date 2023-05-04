59d319e92afc0dd9d12c8bff358cadabe228c2ee
Filip Nordqvist <114819894+Dautabossy@users.noreply.github.com>
2023-05-04 10:48:56 +0200

    js filer i projkten

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const c1Router = require("./v1/routes");
+const v1Router = require("./v1/routes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
